75bc625c399668d7ac7a6dd4b35ff38ba88d3194
Sudarshni Ramesh <shruthiramesh2001@gmail.com>
2021-04-12 20:17:25 +0530

    day4

--- jquery.js
+++ jquery.js
@@ -5,9 +5,21 @@ $(document).ready(function(){
     $('.nav_bar').toggleClass('nav-toggle');
   });
 
+  $('#login').click(function(){
+    $('.login-form').addClass('popup');
+  });
+
+  $('.login-form form .fa-times').click(function(){
+    $('.login-form').removeClass('popup');
+  });
+
   $(window).on('load scroll',function(){
+
       $('#menu').removeClass('fa-times');
       $('.nav_bar').removeClass('nav-toggle');
+
+      $('.login-form').removeClass('popup');
+      
   });
 
 });
